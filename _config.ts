33eeff52be193eb07abd3b904ef374ed05430096
haruyuki@hailfall.macbook <haruyuki@relicsnow.info>
2025-05-31 16:21:55 +0900

    jsxを導入した

--- _config.ts
+++ _config.ts
@@ -1,4 +1,5 @@
 import lume from "lume/mod.ts";
+import jsx from "lume/plugins/jsx.ts";
 
 const lumeOptions: Parameters<typeof lume>[0] = {
   src: "./src",
@@ -6,5 +7,6 @@ const lumeOptions: Parameters<typeof lume>[0] = {
 
 const site = lume(lumeOptions);
 site.add("CNAME")
+site.use(jsx({pageSubExtension: ".page"}))
 
 export default site;
